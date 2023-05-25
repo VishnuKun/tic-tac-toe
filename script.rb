e447376a546c7c4128a3c268aa7090660bd9ad76
Vishnu <vishnukun954AV@protonmail.com>
2023-05-25 18:10:17 +0530

    Added comments.

---
+++ script.rb
@@ -0,0 +1,11 @@
+# tic-tac-toe
+
+# display a 3 by 3 table
+# give each user of the two a change
+# user have to provide position(box) to place the mark in
+# to change the table to mark either 'X' or 'O'
+# after taking user input display 'X' or 'O' in the selected box
+# then choice option should be given to player2
+# continue previous steps until all boxes are filled
+# or on player manages to arrange their marks 
+# in a straight line or diagonally in the table
