e120ec9ba269cb6f4c1c187ff3656e9da2ac44b4
Vishnu <vishnukun954AV@protonmail.com>
2023-09-05 12:07:05 +0530

    display_board method now functional

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,26 +3,42 @@
 # tic-tac-toe
 class Tic_Tac_Toe
   attr_accessor :board
+
   def initialize
-    @board =  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
+
   # checks if the board has any winning combination
   def game_over?(board)
     winning_combinations = [
       [0, 1, 2], [3, 4, 5], [6, 7, 8], # Rows
       [0, 3, 6], [1, 4, 7], [2, 5, 8], # Columns
-      [0, 4, 8], [2, 4, 6]              # Diagonals
+      [0, 4, 8], [2, 4, 6] # Diagonals
     ]
-  
+
     winning_combinations.each do |combination|
       marks = combination.map { |index| board[index] }
       return true if marks.uniq.length == 1 && marks[0] != '' # Check if all marks are the same and not empty
     end
-  
+
     false # No winning pattern found
   end
+
   # place's given mark at the given position on board
   def place_mark(location, mark)
     board[location - 1] = mark
+  end
+
+  # displays the board 
+  def display_board
+    b = @board
+    output = "
+    #{b[0]} | #{b[1]} | #{b[2]}
+    ---------
+    #{b[3]} | #{b[4]} | #{b[5]}
+    ---------
+    #{b[6]} | #{b[7]} | #{b[8]}
+    "
+    return output
   end
 end
