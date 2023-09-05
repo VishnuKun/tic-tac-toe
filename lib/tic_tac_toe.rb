dbaadd2c86282e36d899422f69d518667ca69ad4
Vishnu <vishnukun954AV@protonmail.com>
2023-09-05 10:44:36 +0530

    game_over? method functional now

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,7 +6,19 @@ class Tic_Tac_Toe
   def initialize
     @board = Array.new(9) { |i| i + 1} 
   end
-
+  # checks if the board has any winning combination
   def game_over?(board)
+    winning_combinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], # Rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], # Columns
+      [0, 4, 8], [2, 4, 6]              # Diagonals
+    ]
+  
+    winning_combinations.each do |combination|
+      marks = combination.map { |index| board[index] }
+      return true if marks.uniq.length == 1 && marks[0] != '' # Check if all marks are the same and not empty
+    end
+  
+    false # No winning pattern found
   end
 end
